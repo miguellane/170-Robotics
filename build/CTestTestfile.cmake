1fe5077de5c93158c43195b25c37739e33d61c0b
miguellane <lanemiguel1337@gmail.com>
2017-10-13 17:40:31 -0700

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,17 @@
+# CMake generated Testfile for 
+# Source directory: /home/miguel/Desktop/CSE180/src
+# Build directory: /home/miguel/Desktop/CSE180/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(husky_desktop)
+subdirs(husky_simulator)
+subdirs(husky_msgs)
+subdirs(husky_control)
+subdirs(husky_description)
+subdirs(husky_gazebo)
+subdirs(husky_navigation)
+subdirs(husky_ur5_moveit_config)
+subdirs(husky_viz)
+subdirs(simple)
